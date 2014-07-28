43a6eb0643075c0810dd0ffd260d36ddbefd19f9
Maikel Koek <maikel@h-o.nl>
2014-07-28 10:29:16 +0200

    [FIRE] +: More space between last 2 footer columns

--- app/code/community/FireGento/Pdf/Model/Engine/Abstract.php
+++ app/code/community/FireGento/Pdf/Model/Engine/Abstract.php
@@ -668,7 +668,7 @@ abstract class FireGento_Pdf_Model_Engine_Abstract extends Mage_Sales_Model_Orde
             'register_number' => Mage::helper('firegento_pdf')->__('Register number:'),
             'ceo' => Mage::helper('firegento_pdf')->__('CEO:')
         );
-        $this->_insertFooterBlock($page, $fields, 355, 60, $this->margin['right'] - 365 - 10);
+        $this->_insertFooterBlock($page, $fields, 385, 60, $this->margin['right'] - 365 - 10);
     }
 
     /**
